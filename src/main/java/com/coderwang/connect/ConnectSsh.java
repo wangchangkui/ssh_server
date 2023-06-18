0480afc949b59cff39eb248e13c6a5673f4aba0d
王长奎 <1049835177@qq.com>
2023-06-18 21:07:37 +0800

    add lombok and remove netty connect

--- src/main/java/com/coderwang/connect/ConnectSsh.java
+++ src/main/java/com/coderwang/connect/ConnectSsh.java
@@ -3,36 +3,31 @@ package com.coderwang.connect;
 import com.coderwang.config.ConnectConfig;
 import com.coderwang.config.ReadConfig;
 import com.coderwang.exception.ConnectException;
-import com.coderwang.handler.MySshClientHandler;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.sshd.client.SshClient;
-import org.apache.sshd.client.future.AuthFuture;
-import org.apache.sshd.client.future.ConnectFuture;
+import org.apache.sshd.client.channel.ClientChannel;
+import org.apache.sshd.client.channel.ClientChannelEvent;
 import org.apache.sshd.client.session.ClientSession;
-import org.apache.sshd.common.PropertyResolverUtils;
 
-import java.net.InetSocketAddress;
-import java.util.Collections;
+import java.io.IOException;
+import java.util.EnumSet;
+import java.util.List;
 
 /**
  * @author 29059
  */
+@Slf4j
 public class ConnectSsh {
 
     private final ReadConfig readConfig;
 
-    private  Channel channel;
-
-    private final EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
 
     public ConnectSsh(ReadConfig readConfig) {
         this.readConfig = readConfig;
@@ -45,52 +40,34 @@ public class ConnectSsh {
     public void connectSsh(){
         ConnectConfig connectConfig = readConfig.readConfig();
         try{
-            Bootstrap bootstrap =  new Bootstrap();
-            bootstrap.group(eventLoopGroup)
-                    .channel(NioSocketChannel.class)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-
-                        @Override
-                        protected void initChannel(SocketChannel ch) {
-                            //添加客户端通道的处理器
-                            ch.pipeline().addLast(new NettySshClientHandler());
-                        }
-                    });
-            SshClient sshClient = SshClient.setUpDefaultClient();
-            sshClient.start();
-            ConnectFuture connectFuture =sshClient
-                    .connect(connectConfig.getUserName(), connectConfig.getHost(), connectConfig.getPort())
-                    .verify();
-            connectFuture.await();
-            ClientSession session = connectFuture.getSession();
-            session.addPasswordIdentity(connectConfig.getPassWord());
-            AuthFuture verify = session.auth().verify();
-            verify.await();
-
-
-            ChannelFuture channelFuture = bootstrap.connect(new InetSocketAddress(connectConfig.getHost(), connectConfig.getPort())).sync();
-            Channel channel = channelFuture.channel();
-            // 将SSH会话绑定到Netty通道
-            channel.attr(NettySshClientHandler.SESSION_KEY).set(session);
-            // 等待连接关闭
-            channel.closeFuture().sync();
+            sshConnect(connectConfig);
         }catch (Exception e){
+            e.printStackTrace();
             throw new ConnectException("连接服务器失败了",e);
         }
     }
 
 
-    /**
-     * 关闭连接
-     */
-    public void disConnect(){
-        if(channel != null && channel.isOpen()){
-            channel.flush();
-            try {
-                channel.closeFuture().sync();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+    public void sshConnect(ConnectConfig connectConfig){
+        SshClient sshClient = SshClient.setUpDefaultClient();
+        sshClient.setServerKeyVerifier((clientSession, sshPublicKey, socketAddress) -> true);
+        sshClient.setPasswordIdentityProvider(sessionContext -> List.of(connectConfig.getPassWord()));
+        sshClient.start();
+        ClientSession session = null;
+        try {
+            session = sshClient
+                    .connect(connectConfig.getUserName(),connectConfig.getHost(),connectConfig.getPort())
+                    .verify()
+                    .getSession();
+            session.addPasswordIdentity(connectConfig.getPassWord());
+            if (session.isOpen()) {
+                log.info("连接服务器成功");
             }
+            ClientChannel shell = session.createChannel("shell");
+            shell.open().verify();
+            shell.waitFor(EnumSet.of(ClientChannelEvent.CLOSED), 0L);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
